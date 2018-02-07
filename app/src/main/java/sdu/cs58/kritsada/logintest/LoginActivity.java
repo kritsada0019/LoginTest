5d72d7d6706d3572f2b1dd6cfed80cd3dc379871
krtsada <emmypinklab@gmail.com>
2018-02-07 11:43:43 +0700

    Toast Complete

--- app/src/main/java/sdu/cs58/kritsada/logintest/LoginActivity.java
+++ app/src/main/java/sdu/cs58/kritsada/logintest/LoginActivity.java
@@ -2,12 +2,36 @@ package sdu.cs58.kritsada.logintest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
+
+    //Explicit ประกาศตัวแปร
+    EditText nameEditText,usernameEditText, passwordEditText;
+    Button loginButton;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-    }
-}
+
+        //Initail view ผูกตัวแปร Java ให้รู้จักกับ element บน xml
+        nameEditText = findViewById(R.id.txtName);
+        usernameEditText = findViewById(R.id.txtUsername);
+        passwordEditText = findViewById(R.id.txtPassword);
+        loginButton = findViewById(R.id.btnLogin);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(), "Hallo!! Hello!!", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }  //end method onCreate
+} //end class
